4dc14fc9bc0fbc8bab9f76d4f6eeaa18b6df7156
kosolapiyD <you@example.com>
2017-12-20 22:04:06 +0200

    Gallery jQuery, json

---
+++ gal.js
@@ -0,0 +1,52 @@
+var json_ar = [
+        {
+            name:"BB King",
+            about:"B.B. King (September 16, 1925 – May 14, 2015) was an American blues singer, electric guitarist, songwriter, and record producer. King introduced a sophisticated style of soloing based on fluid string bending and shimmering vibrato that influenced many later electric blues guitarists",
+            image:"bb_king.jpg"
+            
+        },
+        {
+            name:"Buddy Guy",
+            about:"George Buddy Guy (born July 30, 1936[2]) is an American blues guitarist and singer. He is an exponent of Chicago blues and has influenced guitarists including Jimi Hendrix, Eric Clapton, Jimmy Page, Keith Richards, Jeff Beck, John Mayer and Stevie Ray Vaughan. In the 1960s, Guy played with Muddy Waters as a house guitarist at Chess Records and began a musical partnership with harmonica player Junior Wells",
+            image:"buddy_guy.jpg"
+            
+        },
+        {
+            name:"Eric Clapton",
+            about:"Eric Patrick Clapton, CBE (born 30 March 1945), is an English rock and blues guitarist, singer and songwriter. He is the only three-time inductee to the Rock and Roll Hall of Fame: once as a solo artist and separately as a member of the Yardbirds and Cream. Clapton has been referred to as one of the most important and influential guitarists of all time. Clapton ranked second in Rolling Stone magazine's list of the 100 Greatest Guitarists of All Time and fourth in Gibson's Top 50 Guitarists of All Time. He was also named number five in Time magazine's list of The 10 Best Electric Guitar Players in 2009",
+            image:"eric_clapton.jpg"
+            
+        },
+        {
+            name:"Howlin Wolf",
+            about:"Chester Arthur Burnett (June 10, 1910 – January 10, 1976), known as Howlin' Wolf, was an African-American Chicago blues singer, guitarist and harmonica player, originally from Mississippi. With a booming voice and looming physical presence, he is one of the best-known Chicago blues artists. Musician and critic Cub Koda noted, no one could match Howlin' Wolf for the singular ability to rock the house down to the foundation while simultaneously scaring its patrons out of its wits",
+            image:"howlin_wolf.jpg"
+            
+        },
+        {
+            name:"Jimi Hendrix",
+            about:"James Marshall, Jimi Hendrix (born Johnny Allen Hendrix; November 27, 1942 – September 18, 1970) was an American rock guitarist, singer, and songwriter. Although his mainstream career spanned only four years, he is widely regarded as one of the most influential electric guitarists in the history of popular music, and one of the most celebrated musicians of the 20th century. The Rock and Roll Hall of Fame describes him as arguably the greatest instrumentalist in the history of rock music",
+            image:"jimi_hendrix.jpg"
+            
+        },
+        {
+            name:"John Lee Hooker",
+            about:"John Lee Hooker (August 22, 1917 – June 21, 2001) was an American blues singer, songwriter, and guitarist. He was born in Mississippi, the son of a sharecropper, and rose to prominence performing an electric guitar-style adaptation of Delta blues. Hooker often incorporated other elements, including talking blues and early North Mississippi Hill country blues. He developed his own driving-rhythm boogie style, distinct from the 1930s–1940s piano-derived boogie-woogie",
+            image:"john_lee_hooker.jpg"
+            
+        },
+        {
+            name:"Muddy Waters",
+            about:"McKinley Morganfield (April 4, 1913[2] – April 30, 1983), known by his stage name Muddy Waters, was an American blues musician who is often cited as the father of modern Chicago blues",
+            image:"muddy_waters.jpg"
+            
+        },
+        {
+            name:"Stevie Ray Vaughan",
+            about:"Stevie Ray Vaughan (October 3, 1954 – August 27, 1990) was an American musician, singer, songwriter, and record producer. In spite of a short-lived mainstream career spanning seven years, he is widely considered one of the most influential electric guitarists in the history of blues music, and one of the most important figures in the revival of blues in the 1980s",
+            image:"stevie_ray_vaughan.jpg"
+            
+        }
+    ];
+
+
